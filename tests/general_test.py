9f6b403d35b98796bfc0b14fdc852c0b46e822a5
SimonTaurus <simon.stier@gmx.de>
2026-04-28 11:49:48 +0200

    generated code from world.opensemantic.base@0.42.6

--- tests/general_test.py
+++ tests/general_test.py
@@ -1,10 +1,12 @@
 import opensemantic.base
+import opensemantic.base.v1
 import opensemantic.core
+import opensemantic.core.v1
 
 
 def test_opensemantic():
 
-    # Create an instance of OswBaseModel
+    # Create an instance of Organization
     model = opensemantic.base.Organization(
         label=[opensemantic.core.Label(text="Test Entity")],
     )
@@ -12,7 +14,19 @@ def test_opensemantic():
     # Check if the instance is created successfully
     assert isinstance(
         model, opensemantic.base.Organization
-    ), "Failed to create an instance of OswBaseModel"
+    ), "Failed to create an instance of Organization"
+
+    # v1 tests
+
+    # Create an instance of Organization
+    model = opensemantic.base.v1.Organization(
+        label=[opensemantic.core.v1.Label(text="Test Entity")],
+    )
+
+    # Check if the instance is created successfully
+    assert isinstance(
+        model, opensemantic.base.v1.Organization
+    ), "Failed to create an instance of Organization"
 
 
 if __name__ == "__main__":
